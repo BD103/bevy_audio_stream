30a8536025d166a6973771b9e3ea079fd287472d
BD103 <59022059+BD103@users.noreply.github.com>
2024-03-23 15:42:05 -0400

    feat: doc and test audio stream

--- crates/bevy_audio_stream/src/source.rs
+++ crates/bevy_audio_stream/src/source.rs
@@ -9,9 +9,37 @@ use std::sync::Arc;
 ///
 /// You can create a new stream with [`AudioStreamSource::new`], and send samples to it using [`AudioStreamSource::sender`].
 ///
-/// This type is parallel to [`AudioSource`](bevy_audio::AudioSource), in the sense that you can use it in an [`AudioSourceBundle`](crate::AudioStreamBundle) to play audio.
+/// This type is parallel to [`AudioSource`](bevy_audio::AudioSource) in the sense that you can use it in an [`AudioSourceBundle`](crate::AudioStreamBundle) to play audio.
 ///
 /// This stream is lossy. If it runs out of capacity for new [`StreamChunk`]s, it will discard them.
+///
+/// # Example
+///
+/// ```
+/// # use bevy::prelude::*;
+/// # use bevy_audio_stream::{AudioStreamBundle, AudioStreamSender, AudioStreamSource};
+/// #
+/// #[derive(Resource, Deref)]
+/// struct MyAudioSender(AudioStreamSender);
+///
+/// fn setup(mut commands: Commands, mut audio: ResMut<Assets<AudioStreamSource>>) {
+///     // Create a new audio stream.
+///     let stream = AudioStreamSource::new(4, 1, 44100);
+///
+///     // Add the stream sender as a resource, for later access.
+///     commands.insert_resource(MyAudioSender(stream.sender()));
+///
+///     // Spawn a new entity that will play the audio stream.
+///     commands.spawn(AudioStreamBundle {
+///         source: audio.add(stream),
+///         ..default()
+///     });
+/// }
+/// #
+/// # fn check_valid_system<M>(_system: impl IntoSystemConfigs<M>) {}
+/// #
+/// # check_valid_system(setup);
+/// ```
 #[derive(Asset, TypePath)]
 pub struct AudioStreamSource {
     stream: Arc<ArrayQueue<StreamChunk>>,
@@ -42,5 +70,33 @@ impl Decodable for AudioStreamSource {
 
     fn decoder(&self) -> Self::Decoder {
         AudioStreamDecoder::new(Arc::clone(&self.stream), self.channels, self.sample_rate)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy_audio::Source;
+
+    use super::*;
+
+    #[test]
+    fn receives_audio() {
+        const CHANNELS: u16 = 1;
+        const SAMPLE_RATE: u32 = 44100;
+
+        let stream = AudioStreamSource::new(1, CHANNELS, SAMPLE_RATE);
+        let sender = stream.sender();
+        let decoder = stream.decoder();
+
+        // Assert settings persist from source to decoder.
+        assert_eq!(decoder.channels(), CHANNELS);
+        assert_eq!(decoder.sample_rate(), SAMPLE_RATE);
+
+        // Assert sent audio is received by decoder.
+        assert!(
+            sender.send([0.8; 512]),
+            "Failed to send audio through stream, even with sufficient capacity."
+        );
+        assert!(decoder.take(512).all(|x| x == 0.8));
     }
 }
