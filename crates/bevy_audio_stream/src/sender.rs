fcf77841b905b5a6ad3acb899f4cacff93db589d
BD103 <59022059+BD103@users.noreply.github.com>
2024-03-23 15:23:09 -0400

    feat: doc and test audio stream sender

--- crates/bevy_audio_stream/src/sender.rs
+++ crates/bevy_audio_stream/src/sender.rs
@@ -5,6 +5,21 @@ use std::sync::Arc;
 /// A sender that sends audio through a stream.
 ///
 /// Create a new sender by calling [`AudioStreamSource::sender`](crate::AudioStreamSource::sender).
+///
+/// # Example
+///
+/// ```
+/// # use bevy_audio_stream::AudioStreamSource;
+/// #
+/// // Create a new audio stream.
+/// let stream = AudioStreamSource::new(4, 1, 44100);
+///
+/// // Create a new sender for the associated stream.
+/// let sender = stream.sender();
+///
+/// // Send new audio through the stream like this:
+/// sender.send([0.5; 512]);
+/// ```
 #[repr(transparent)]
 pub struct AudioStreamSender {
     stream: Arc<ArrayQueue<StreamChunk>>,
@@ -16,6 +31,8 @@ impl AudioStreamSender {
     }
 
     /// Sends to chunk of audio through the streaming, returning true if successful.
+    ///
+    /// This will fail and return false if the stream is full and does not have a large enough capacity.
     pub fn send(&self, chunk: StreamChunk) -> bool {
         self.stream.push(chunk).is_ok()
     }
@@ -26,5 +43,34 @@ impl Clone for AudioStreamSender {
         AudioStreamSender {
             stream: Arc::clone(&self.stream),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn send() {
+        // Create sender with capacity of 2.
+        let sender = AudioStreamSender::new(Arc::new(ArrayQueue::new(2)));
+
+        // First two chunks should succeed.
+        assert!(sender.send([1.0; 512]));
+        assert!(sender.send([1.0; 512]));
+
+        // Third chunk should fail, not enough capacity!
+        assert!(!sender.send([1.0; 512]));
+    }
+
+    #[test]
+    fn clone() {
+        let sender = AudioStreamSender::new(Arc::new(ArrayQueue::new(1)));
+        let sender_clone = sender.clone();
+
+        assert!(
+            Arc::ptr_eq(&sender.stream, &sender_clone.stream),
+            "Cloned audio stream sender does not share the same underlying memory."
+        );
     }
 }
