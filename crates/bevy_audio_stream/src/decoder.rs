a95abe8e9d66dc86abc214292e3775a328a24d44
BD103 <59022059+BD103@users.noreply.github.com>
2024-03-23 13:04:17 -0400

    feat: test audio stream decoder

--- crates/bevy_audio_stream/src/decoder.rs
+++ crates/bevy_audio_stream/src/decoder.rs
@@ -81,3 +81,81 @@ impl Source for AudioStreamDecoder {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn receives_stream() {
+        // Create a new stream and associated decoder.
+        let stream = Arc::new(ArrayQueue::new(2));
+        let decoder = AudioStreamDecoder::new(Arc::clone(&stream), 1, 44100);
+
+        // Push a chunk of all ones, then all halves.
+        stream.push([1.0; 512]).unwrap();
+        stream.push([0.5; 512]).unwrap();
+
+        // Iterate over the decoder and collect both chunks into a `Vec`.
+        let samples: Vec<_> = decoder.take(512 * 2).collect();
+        let (all_ones, all_halves) = samples.split_at(512);
+
+        // Check that data is intact.
+        assert!(all_ones.iter().all(|&x| x == 1.0));
+        assert!(all_halves.iter().all(|&x| x == 0.5));
+    }
+
+    #[test]
+    fn empty_stream() {
+        // Create a new stream and associated decoder.
+        let stream = Arc::new(ArrayQueue::new(1));
+        let mut decoder = AudioStreamDecoder::new(Arc::clone(&stream), 1, 44100);
+
+        // If there is nothing in the stream, it should return silence.
+        assert_eq!(
+            decoder.next(),
+            Some(0.0),
+            "Empty decoder stream did not return silence."
+        );
+
+        // Send something in the stream.
+        stream.push([1.0; 512]).unwrap();
+
+        assert_eq!(
+            decoder.next(),
+            Some(1.0),
+            "Decoder stream did not receiver new audio."
+        );
+    }
+
+    #[test]
+    fn index() {
+        // Create a new stream and associated decoder.
+        let stream = Arc::new(ArrayQueue::new(1));
+        let mut decoder = AudioStreamDecoder::new(Arc::clone(&stream), 1, 44100);
+
+        let original_index = decoder.i;
+
+        // Original index needs to be >= the length of a chunk, so the decoder pops a new chunk from the stream next time `.next()` is called.
+        assert_eq!(
+            original_index, 512,
+            "Decoder stream original index is not the length of `StreamChunk`."
+        );
+
+        decoder.next();
+
+        // If i increments with an empty stream, then it could wrap around to 0 and replace audio.
+        assert_eq!(
+            decoder.i, original_index,
+            "Decoder stream index incremented even while empty."
+        );
+
+        stream.push([0.0; 512]).unwrap();
+        decoder.next();
+
+        assert_eq!(
+            decoder.i, 1,
+            "Decoder stream index did not change after receiving audio."
+        );
+    }
+}
