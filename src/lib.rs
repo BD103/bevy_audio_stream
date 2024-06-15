5cb0be4dede2413f9fe85c811c903f886a92d731
BD103 <59022059+BD103@users.noreply.github.com>
2024-06-15 19:28:57 -0400

    chore!: reset everything for 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,1 @@
 #![doc = include_str!("../README.md")]
-
-mod decoder;
-mod plugin;
-mod sender;
-mod source;
-
-pub use self::{
-    decoder::AudioStreamDecoder, plugin::AudioStreamPlugin, sender::AudioStreamSender,
-    source::AudioStreamSource,
-};
-
-/// A chunk of samples sent in a stream.
-pub type StreamChunk = [f32; 512];
-
-/// Bundle for playing streamed audio.
-pub type AudioStreamBundle = bevy_audio::AudioSourceBundle<AudioStreamSource>;
